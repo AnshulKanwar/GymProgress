5c97b927a61296bbf9315cfb4774db94435c3913
Anshul Kanwar <4n5hu1@protonmail.com>
2023-06-08 14:27:37 +0530

    Initial Commit

---
+++ GymProgress/GymProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymProgressApp.swift
+//  GymProgress
+//
+//  Created by Anshul Kanwar on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GymProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
